e07f75d4f392307911a4dee6ba5193395874dadf
RamanPan <ramanpan123@gmail.com>
2022-09-24 08:48:00 +0300

    Big code refactoring

--- CalculatorWithArrowControl/main.c
+++ CalculatorWithArrowControl/main.c
@@ -1,115 +1,140 @@
 #include <stdio.h>
 #include "functionality.h"
-#include <windows.h>
 #include <stdbool.h>
+#include <conio.h>
 
 int main() {
     long firstValue;
     long secondValue;
     int power;
-    short minPosition = 1, maxPosition = 8;
-    short position = 0;
+    short minPosition = 1, maxPosition = 10;
+    short position = minPosition;
+    int symbol;
+    char YN;
+    long swap;
     bool exitFlag = false;
-    bool permissionToInput = false;
-    bool permissionToChoice = false;
-    printf("Welcome to Calculator without shit\n");
-    printf("Enter the number\n");
+    bool permissionFlag;
+    printf("Welcome to Calculator!\n");
+    printf("Enter first number\n");
     firstValue = validation();
+    printf("Enter second number\n");
+    secondValue = validation();
+    showMenu(position, firstValue, secondValue);
     while (!exitFlag) {
-        if (GetAsyncKeyState(VK_UP) && permissionToInput) {
-            keybd_event(VK_UP, 0, KEYEVENTF_KEYUP, 0);//Отжимаем кнопку
-            if (0 != position - 1)
-                position--;
-            else
-                position = maxPosition;
+        permissionFlag = false;
+        symbol = getch();
+        if (symbol == 224) symbol = getch();
+        switch (symbol) {
+            case 72:
+                if (0 != position - 1)
+                    position--;
+                else position = maxPosition;
+                break;
+            case 80:
+                if (position < maxPosition) position++;
+                else
+                    position = minPosition;
+                break;
+            case 27:
+                scanf("%s", &YN);
+                if (YN == 'Y') exitFlag = true;
+                break;
+            case 13:
+                permissionFlag = true;
+                break;
+            case 48:
+                position = 10;
+                break;
+            case 49:
+                position = 1;
+                break;
+            case 50:
+                position = 2;
+                break;
+            case 51:
+                position = 3;
+                break;
+            case 52:
+                position = 4;
+                break;
+            case 53:
+                position = 5;
+                break;
+            case 54:
+                position = 6;
+                break;
+            case 55:
+                position = 7;
+                break;
+            case 56:
+                position = 8;
+                break;
+            case 57:
+                position = 9;
+                break;
+            default:;
         }
-        if (GetAsyncKeyState(VK_DOWN) && permissionToInput) {
-            keybd_event(VK_DOWN, 0, KEYEVENTF_KEYUP, 0);
-            if (position < maxPosition)
-                position++;
-            else
-                position = minPosition;
-        }
-        permissionToInput = true;
-        if (GetAsyncKeyState(VK_RETURN)) {
-            keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
-            if (permissionToChoice) {
-                switch (position) {
-                    case 1:
-                        printf("Enter the second number for '+'\n");
-                        secondValue = validation();
-                        permissionToInput = false;
-                        permissionToChoice = false;
-                        firstValue = add(firstValue, secondValue);
-                        printf("Result = %ld\n", firstValue);
-                        break;
-                    case 2:
-                        printf("Enter the second number for '*'\n");
-                        secondValue = validation();
-                        permissionToInput = false;
-                        permissionToChoice = false;
-                        firstValue = multiply(firstValue, secondValue);
-                        printf("Result = %ld\n", firstValue);
-                        break;
-                    case 3:
-                        printf("Enter the second number for '-'\n");
-                        secondValue = validation();
-                        permissionToInput = false;
-                        permissionToChoice = false;
-                        firstValue = subtraction(firstValue, secondValue);
-                        printf("Result = %ld\n", firstValue);
-                        break;
-                    case 4:
-                        printf("Enter the second number for '/'\n");
-                        permissionToInput = false;
-                        permissionToChoice = false;
-                        firstValue = division(firstValue, secondValue);
-                        break;
-                    case 5:
-                        printf("Enter the power\n");
+        showMenu(position, firstValue, secondValue);
+        if (permissionFlag) {
+            switch (position) {
+                case 1:
+                    printf("Result = %ld\n", add(firstValue, secondValue));
+                    break;
+                case 2:
+                    printf("Result = %ld\n", multiply(firstValue, secondValue));
+                    break;
+                case 3:
+                    printf("Result = %ld\n", subtraction(firstValue, secondValue));
+                    break;
+                case 4:
+                    if (validationForDivide(secondValue))
+                        printf("Result = %ld\n", division(firstValue, secondValue));
+                    break;
+                case 5:
+                    printf("Enter the power\n");
+                    power = validation();
+                    exponentiation(firstValue, power);
+                    break;
+                case 6:
+                    if (validationForRoot(firstValue)) {
+                        printf("Enter the power root\n");
                         power = validation();
-                        permissionToInput = false;
-                        permissionToChoice = false;
-                        firstValue = (long) exponentiation(firstValue, power);
-                        break;
-                    case 6:
-                        if (validationForRoot(firstValue)) {
-                            printf("Enter the power root\n");
-                            power = validation();
-                            permissionToInput = false;
-                            permissionToChoice = false;
-                            if (power <= 0) {
-                                printf("Wrong power! Power less or equals zero! Try again");
-                                break;
-                            }
-                            firstValue = (long) rooting(firstValue, power);
-                        } else printf("Wrong value! Value less then zero! Change it and try again");
-                        break;
-                    case 7:
-                        printf("Enter the number\n");
-                        firstValue = validation();
-                        permissionToInput = false;
-                        permissionToChoice = false;
-                        printf("Result = %ld\n", firstValue);
-                        break;
-                    case 8:
-                        printf("Are you sure you wanna exit?(Y/N)\n");
-                        char YN;
-                        scanf("%s", &YN);
-                        if (YN == 'Y') exitFlag = true;
-                        break;
-                    default:
-                        printf("It's impossible but you seeing this text now");
-                }
-            } else permissionToChoice = true;
-        }
-        if (GetAsyncKeyState(VK_ESCAPE)) {
-            keybd_event(VK_ESCAPE, 0, KEYEVENTF_KEYUP, 0);
-            printf("See you soon!");
-            return 0;
+                        if (power <= 0) {
+                            printf("Wrong power! Power less or equals zero! Try again");
+                            break;
+                        }
+                        rooting(firstValue, power);
+                    } else printf("Wrong value! Value less then zero! Change it and try again");
+                    break;
+                case 7:
+                    printf("Enter the number for change\n");
+                    firstValue = validation();
+                    showMenu(position, firstValue, secondValue);
+                    printf("First value changed\n");
+                    break;
+                case 8:
+                    printf("Enter the number for change\n");
+                    secondValue = validation();
+                    showMenu(position, firstValue, secondValue);
+                    printf("Second value changed\n");
+                    break;
+                case 9:
+                    swap = firstValue;
+                    firstValue = secondValue;
+                    secondValue = swap;
+                    showMenu(position, firstValue, secondValue);
+                    printf("Values was changed successfully");
+                    break;
+                case 10:
+                    printf("Are you sure you wanna exit?(Y/N)\n");
+                    scanf("%s", &YN);
+                    if (YN == 'Y') exitFlag = true;
+                    break;
+                default:
+                    break;
+            }
         }
     }
     printf("See you soon!");
     return 0;
 }
-
