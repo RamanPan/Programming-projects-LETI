f01a3f032ff8de34ed61bf3d14168c3c167bbcfc
RamanPan <ramanpan123@gmail.com>
2022-10-14 10:07:27 +0300

    Validation done

--- Tic-tac-toe/functionality.c
+++ Tic-tac-toe/functionality.c
@@ -1,2 +1,35 @@
-
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "functionality.h"
+#include "functionality.h"
+
+long validation() {
+    bool validationFlag = false;
+    double result;
+    while (!validationFlag) {
+        if (scanf("%lf", &result)) {
+            if (!checkOverflow(result)) {
+                printf("Неправильный ввод! Число вышло за границу дозволенного! Попробуйте снова!\n");
+            } else validationFlag = true;
+        } else printf("Неправильный ввод! Попробуйте снова!\n");
+        fflush(stdin);
+    }
+    return (long) result;
+}
+
+long validationGameArea() {
+    long result;
+    short minArea = 2, maxArea = 14;
+    do {
+        result = validation();
+        if (result > maxArea || result < minArea)
+            printf("Неверный ввод размера поля! Размер поля может принимать значения от %hd до %hd\n", minArea,
+                   maxArea);
+    } while (result > maxArea || result < minArea);
+    return result;
+}
+
+bool checkOverflow(double d) {
+    return d >= -2147483648 && d <= 2147483647;
+}
--- Tic-tac-toe/functionality.h
+++ Tic-tac-toe/functionality.h
@@ -2,4 +2,12 @@
 #ifndef TIC_TAC_TOE_FUNCTIONALITY_H
 #define TIC_TAC_TOE_FUNCTIONALITY_H
 
+void showHelloMessage();
+
+long validation();
+
+long validationGameArea();
+
+bool checkOverflow(double d);
+
 #endif //TIC_TAC_TOE_FUNCTIONALITY_H
