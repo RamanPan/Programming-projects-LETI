5235708757ca46bfc2d77f54841535a31b95dfaf
RamanPan <ramanpan123@gmail.com>
2022-10-15 17:16:41 +0300

    Update validation and add hello messages to user

--- Tic-tac-toe/functionality.c
+++ Tic-tac-toe/functionality.c
@@ -30,6 +30,27 @@ long validationGameArea() {
     return result;
 }
 
+long validationWithArgument(short max) {
+    long result;
+    short minArea = 2, maxArea = max;
+    do {
+        result = validation();
+        if (result > maxArea || result < minArea)
+            printf("Неверный ввод размера поля! Размер поля может принимать значения от %hd до %hd\n", minArea,
+                   maxArea);
+    } while (result > maxArea || result < minArea);
+    return result;
+}
+
+void showHelloMessage() {
+    printf("Приветствую!\n");
+    printf("Это игра 'Крестики и нолики'\n");
+    printf("Перед игрой вам нужно ввести высоту и ширину игрового поля\n");
+    printf("Затем выбрать кол-во очков для победы крестиков и для победы ноликов\n");
+    printf("Очки для победы не могут быть больше чем высота и ширина поля\n");
+    printf("Если на поле не осталось места - игра завершается ничьей\n");
+}
+
 bool checkOverflow(double d) {
     return d >= -2147483648 && d <= 2147483647;
 }
