79cbbbcda3c26d04c14402bba591b01db728942a
RamanPan <ramanpan123@gmail.com>
2022-10-15 17:15:24 +0300

    A set of functions is defined, the implementation of which is necessary to complete the task

--- Tic-tac-toe/functionality.h
+++ Tic-tac-toe/functionality.h
@@ -1,4 +1,3 @@
-
 #ifndef TIC_TAC_TOE_FUNCTIONALITY_H
 #define TIC_TAC_TOE_FUNCTIONALITY_H
 
@@ -8,10 +7,25 @@ long validation();
 
 long validationGameArea();
 
+long validationWithArgument(short max);
+
 bool checkOverflow(double d);
 
-void cleanArea(int n, int area[][n]);
+void cleanArea(int n, int m, int area[][m]);
 
-void fillingArea(int n, int area[][n]);
+void moveCursor(int m, int area[][m], int positionCursor[], short orientation);
+
+void setXorO(int m, int area[][m], int gameData[], int positionCursor[], int positionX[], int positionO[],
+             bool whoseStep);
+
+void fillingArea(int n, int m, int area[n][m]);
+
+void printSymbol(short numberSymbol);
+
+void showMenu(int n, int m, int gameData[], int area[][m], int pointsForWinX, int pointsForWin0, bool XorO);
+
+bool checkWin(int m, int area[][m], int statementForWin, bool XorO);
+
+bool checkForMove(int n, int area[][n], int x, int y);
 
 #endif //TIC_TAC_TOE_FUNCTIONALITY_H
