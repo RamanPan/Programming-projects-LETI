94a13b347fd4cda6e294c7bc09ef436b0dc85b2e
RamanPan <ramanpan123@gmail.com>
2022-11-12 18:42:54 +0300

    Add and subtract matrix done

--- OperationsOnMatrices/functionality.cpp
+++ OperationsOnMatrices/functionality.cpp
@@ -1,5 +1,20 @@
-//
-// Created by vpana on 11.11.2022.
-//
-
+#include <cstdio>
+#include <conio.h>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 #include "functionality.h"
+#include "constants.h"
+
+using namespace std;
+
+template<typename Type>
+void outputMatrix(const int *N, const int *M, Type matrix[][MAX_MATRIX_SIZE]) {
+    for (int i = 0; i < *N; ++i)
+        for (int j = 0; j < *M; ++j) {
+            cout.precision(3);
+            cout << setfill(' ') << setw(7) << matrix[i][j] << " ";
+            if (j == *M - 1) printf("\n");
+        }
+}
+
