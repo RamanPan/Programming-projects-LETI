c7f71264827869b4746ef21e5ff5e0414f6a25a2
RamanPan <ramanpan123@gmail.com>
2022-11-12 11:48:25 +0300

    All necessary functions defined

--- OperationsOnMatrices/functionality.h
+++ OperationsOnMatrices/functionality.h
@@ -1,27 +1,49 @@
 #include "constants.h"
+#include <windows.h>
+
 #ifndef OPERATIONSONMATRICES_FUNCTIONALITY_H
 #define OPERATIONSONMATRICES_FUNCTIONALITY_H
-void showMenu(int position, int N, int M, int R, int K);
 
 long validation();
 
-long validationWithArgument(short max);
+long validationWithArgument(short min, short max);
 
 void consoleInterface();
 
+void showMenu(int position, const int *N, const int *M, const int *R, const int *K);
+
 void showHelloMessage();
 
-void randomizeValuesInMatrix(int N,int M, int matrix[][MAX_MATRIX_SIZE]);
+void randomizeValuesInMatrix(const int *N, const int *M, int matrix[][MAX_MATRIX_SIZE]);
 
-void initValuesInMatrix(int N,int M, int matrix[][MAX_MATRIX_SIZE]);
+void initValuesInMatrix(const int *N, const int *M, int matrix[][MAX_MATRIX_SIZE]);
 
-void outputMatrix(int N, int M, int matrix[][MAX_MATRIX_SIZE]);
+void transposeMatrix(const int *N, const int *M, int matrix[][MAX_MATRIX_SIZE]);
 
-void calculateDeterminant(int N, int M, int matrix[][MAX_MATRIX_SIZE]);
+void outputMatrix(const int *N, const int *M, int matrix[][MAX_MATRIX_SIZE]);
 
-void calculateInverseMatrix(int N, int M, int matrix[][MAX_MATRIX_SIZE]);
+void calculateAdditionMatrix(const int *N, int matrix[][MAX_MATRIX_SIZE], int additionMatrix[][MAX_MATRIX_SIZE]);
 
-void multiplyMatrix(int firstN, int firstM, int firstMatrix[][MAX_MATRIX_SIZE], int secondN, int secondM, int secondMatrix[][MAX_MATRIX_SIZE]);
+void initHollowMatrix(const int *N, const int *M, int matrix[][MAX_MATRIX_SIZE]);
 
-void addMatrix(int firstN, int firstM, int firstMatrix[][MAX_MATRIX_SIZE], int secondN, int secondM, int secondMatrix[][MAX_MATRIX_SIZE]);
+void createMatrix(int &N, int &M, int matrix[][MAX_MATRIX_SIZE]);
+
+int calculateDeterminant(int **M, int N);
+
+int **getM(int **M, int N, int k);
+
+void choice(char &YN);
+
+int **castToPointer(int N, int M, int matrix[][MAX_MATRIX_SIZE]);
+
+void calculateInverseMatrix(const int *N, const int *M, int D, int matrix[][MAX_MATRIX_SIZE]);
+
+void multiplyMatrix(const int *firstN, const int *firstM, int firstMatrix[][MAX_MATRIX_SIZE], const int *secondN,
+                    const int *secondM,
+                    int secondMatrix[][MAX_MATRIX_SIZE]);
+
+void addOrSubtractMatrix(const int *firstN, const int *firstM, int firstMatrix[][MAX_MATRIX_SIZE], const int *secondN,
+               const int *secondM,
+               int secondMatrix[][MAX_MATRIX_SIZE]);
+
 #endif //OPERATIONSONMATRICES_FUNCTIONALITY_H
