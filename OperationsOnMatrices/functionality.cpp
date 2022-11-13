455836da6a5236280dd94e1eab7e222e64f0e530
RamanPan <ramanpan123@gmail.com>
2022-11-13 11:10:28 +0300

    Validation ready

--- OperationsOnMatrices/functionality.cpp
+++ OperationsOnMatrices/functionality.cpp
@@ -43,60 +43,60 @@ bool checkOverflow(double d) {
     return d >= -2147483648 && d <= 2147483647;
 }
 
-//void choice(char &YN) {
-//    bool exit = false;
-//    while (!exit) {
-//        YN = getchar();
-//        fflush(stdin);
-//        if (YN == '1' || YN == '2') exit = true;
-//        else if (YN != ENTER) {
-//            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-//            printf("Неверный ввод! Значение должно быть либо 1, либо 2 \n");
-//            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-//        }
-//    }
-//}
-//
-//long validation() {
-//    bool validationFlag = false;
-//    double result;
-//    while (!validationFlag) {
-//        if (scanf("%lf", &result)) {
-//            if (!checkOverflow(result)) {
-//                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-//                printf("Неправильный ввод! Число вышло за границу дозволенного! Попробуйте снова!\n");
-//                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-//            } else validationFlag = true;
-//        } else {
-//            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-//            printf("Неправильный ввод! Попробуйте снова!\n");
-//            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-//        }
-//        fflush(stdin);
-//    }
-//    return (long) result;
-//}
-//
-//long validationWithArgument(short min, short max) {
-//    long result;
-//    do {
-//        result = validation();
-//        if (result > max || result < min) {
-//            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-//            printf("Неверный ввод! Значение должно быть от %hd до %hd\n", min,
-//                   max);
-//            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-//        }
-//    } while (result > max || result < min);
-//    return result;
-//}
-//
-//void initHollowMatrix(const int *N, const int *M, int **matrix) {
-//    for (int i = 0; i < *N; i++)
-//        for (int j = 0; j < *M; j++)
-//            matrix[i][j] = 0;
-//}
-//
+void choice(char &YN) {
+    bool exit = false;
+    while (!exit) {
+        YN = getchar();
+        fflush(stdin);
+        if (YN == '1' || YN == '2') exit = true;
+        else if (YN != ENTER) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неверный ввод! Значение должно быть либо 1, либо 2 \n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+    }
+}
+
+long validation() {
+    bool validationFlag = false;
+    double result;
+    while (!validationFlag) {
+        if (scanf("%lf", &result)) {
+            if (!checkOverflow(result)) {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+                printf("Неправильный ввод! Число вышло за границу дозволенного! Попробуйте снова!\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+            } else validationFlag = true;
+        } else {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неправильный ввод! Попробуйте снова!\n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+        fflush(stdin);
+    }
+    return (long) result;
+}
+
+long validationWithArgument(short min, short max) {
+    long result;
+    do {
+        result = validation();
+        if (result > max || result < min) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неверный ввод! Значение должно быть от %hd до %hd\n", min,
+                   max);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+    } while (result > max || result < min);
+    return result;
+}
+
+void initHollowMatrix(const int *N, const int *M, int **matrix) {
+    for (int i = 0; i < *N; i++)
+        for (int j = 0; j < *M; j++)
+            matrix[i][j] = 0;
+}
+
 //void createMatrix(int &N, int &M, int **matrix) {
 //    char YN;
 //    printf("Введите кол-во строк\n");
