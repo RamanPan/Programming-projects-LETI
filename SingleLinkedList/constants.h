4181bb4b3fdfebadaaa12f85e4326d95720ea9fe
RamanPan <ramanpan123@gmail.com>
2023-02-18 12:27:19 +0300

    Add constants for the interface to work

---
+++ SingleLinkedList/constants.h
@@ -0,0 +1,22 @@
+#ifndef SINGLELINKEDLIST_CONSTANTS_H
+#define SINGLELINKEDLIST_CONSTANTS_H
+#define W 119
+#define S 115
+#define FIRST_BYTE_ARROW 224
+#define ARROW_UP 72
+#define ARROW_DOWN 80
+#define ESC 27
+#define ENTER 13
+#define ZERO 48
+#define ONE 49
+#define TWO 50
+#define THREE 51
+#define FOUR 52
+#define FIVE 53
+#define SIX 54
+#define SEVEN 55
+#define EIGHT 56
+#define NINE 57
+#define MIN_ARROW_POSITION 1
+#define MAX_ARROW_POSITION 9
+#endif //SINGLELINKEDLIST_CONSTANTS_H
