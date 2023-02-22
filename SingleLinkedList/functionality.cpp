14227f9ca016bfa764da5abea362a0c0df7bcaf0
RamanPan <ramanpan123@gmail.com>
2023-02-22 10:07:06 +0300

    Implemented functions for validation and get elements by index

--- SingleLinkedList/functionality.cpp
+++ SingleLinkedList/functionality.cpp
@@ -240,6 +240,69 @@ Element *addElementToList(int index, Element *startList, Element *elementBefore,
 }
 
 
+int getIndex(int &lengthList) {
+    printf("Введите индекс(от 0 до %d)\n", lengthList - 1);
+    return validationWithArgument(0, lengthList - 1);
+}
+
+
+int getIndexForAdd(int &lengthList) {
+    printf("Введите индекс(от -1 до %d, где -1 - добавление в самое начало)\n", lengthList - 1);
+    return validationWithArgument(-1, lengthList - 1);
+}
+
+long validation() {
+    bool validationFlag = false;
+    double result;
+    while (!validationFlag) {
+        if (scanf("%lf", &result)) {
+            if (!checkOverflow(result)) {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+                printf("Неправильный ввод! Число вышло за границу дозволенного! Попробуйте снова!\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+            } else validationFlag = true;
+        } else {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неправильный ввод! Попробуйте снова!\n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+        fflush(stdin);
+    }
+    return (long) result;
+}
+
+long validationWithArgument(int min, int max) {
+    long result;
+    do {
+        result = validation();
+        if (result > max || result < min) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неверный ввод! Значение должно быть от %d до %d\n", min,
+                   max);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+    } while (result > max || result < min);
+    return result;
+}
+
+void choice(char &YN) {
+    bool exit = false;
+    while (!exit) {
+        YN = (char) getchar();
+        fflush(stdin);
+        if (YN == '1' || YN == '2') exit = true;
+        else if (YN != ENTER) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неверный ввод! Значение должно быть либо 1, либо 2 \n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+    }
+}
+
+bool checkOverflow(double d) {
+    return d >= -2147483648 && d <= 2147483647;
+}
+
 
 
 
