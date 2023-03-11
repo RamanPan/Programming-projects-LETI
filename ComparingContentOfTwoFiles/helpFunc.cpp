6e29a8fa4da4511848aead840f8b542396331552
RamanPan <ramanpan123@gmail.com>
2023-03-11 10:54:32 +0300

    Add helpful functions

---
+++ ComparingContentOfTwoFiles/helpFunc.cpp
@@ -0,0 +1,12 @@
+#include <sstream>
+#include "helpFunc.h"
+
+std::vector <std::string> split(const std::string &s, char delimiter) {
+    std::vector <std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
---
+++ ComparingContentOfTwoFiles/helpFunc.h
@@ -0,0 +1,15 @@
+
+#ifndef COMPARINGCONTENTOFTWOFILES_HELPFUNC_H
+#define COMPARINGCONTENTOFTWOFILES_HELPFUNC_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <utility>
+#include <vector>
+
+std::vector<std::string> split(const std::string &s, char delimiter);
+
+
+#endif //COMPARINGCONTENTOFTWOFILES_HELPFUNC_H
