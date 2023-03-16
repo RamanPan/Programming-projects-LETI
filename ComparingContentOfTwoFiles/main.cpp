2f7b0b32b9ef0b5408ac5fd2f9d23d1b31e13394
RamanPan <ramanpan123@gmail.com>
2023-03-16 09:49:45 +0300

    Done coding in main

--- ComparingContentOfTwoFiles/main.cpp
+++ ComparingContentOfTwoFiles/main.cpp
@@ -1,19 +1,25 @@
 #include <conio.h>
 #include "functionality.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    system("mode con cols=200 lines=50");
     SetConsoleOutputCP(CP_UTF8);
-    ifstream firstFile = getFileWithCheckExists(), secondFile = getFileWithCheckExists();
+    cout << *argv;
+    ifstream firstFile = getFileWithCheckExists(argv[1]), secondFile = getFileWithCheckExists(argv[2]);
     vector<string> allLinesFirstFile = readAllLinesFromFile(firstFile), allLinesSecondFile = readAllLinesFromFile(
             secondFile);
+    firstFile.close();
+    secondFile.close();
+    vector<Word> wordsFromFirstFile, wordsFromSecondFile;
+    getAllWordsFromLines(allLinesFirstFile, wordsFromFirstFile);
+    getAllWordsFromLines(allLinesSecondFile, wordsFromSecondFile);
     bool exit = false;
     short symbol;
+    findDiff(wordsFromFirstFile, wordsFromSecondFile);
     while (!exit) {
         system("cls");
-        showMenu(allLinesFirstFile, allLinesSecondFile);
+        showMenu(wordsFromFirstFile, wordsFromSecondFile);
         symbol = getch();
         if (symbol == ESC) exit = true;
     }
-    firstFile.close();
-    secondFile.close();
 }
