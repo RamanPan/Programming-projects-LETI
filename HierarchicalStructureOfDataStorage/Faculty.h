086ea1154bb5eb05a9dfd5f55994182762c34f13
RamanPan <ramanpan123@gmail.com>
2023-03-20 00:10:36 +0300

    Class Faculty almost done

---
+++ HierarchicalStructureOfDataStorage/Faculty.h
@@ -0,0 +1,24 @@
+#ifndef HIERARCHICALSTRUCTUREOFDATASTORAGE_FACULTY_H
+#define HIERARCHICALSTRUCTUREOFDATASTORAGE_FACULTY_H
+
+#include <string>
+#include "Department.h"
+
+class Faculty {
+private:
+    std::string title;
+    std::vector<Department> departments;
+public:
+    explicit Faculty(const std::string &title);
+
+    Faculty();
+
+    const std::string &getTitle() const;
+
+    void setTitle(const std::string &title);
+
+    const std::vector<Department> &getDepartments() const;
+
+    void setDepartments(const std::vector<Department> &departments);
+};
+#endif //HIERARCHICALSTRUCTUREOFDATASTORAGE_FACULTY_H
