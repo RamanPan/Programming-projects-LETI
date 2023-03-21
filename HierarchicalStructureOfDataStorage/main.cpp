eb237eaf20f838cd26c303a825947b086c9c8e85
RamanPan <ramanpan123@gmail.com>
2023-03-21 09:40:52 +0300

    Changes im CMakeLists for work static libraries

--- HierarchicalStructureOfDataStorage/main.cpp
+++ HierarchicalStructureOfDataStorage/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "University.h"
+#include "supportFunctions.h"
 
 int main() {
-
+    University university("Saint Petersburg Electrotechnical University \"LETI\"");
+    consoleInterfaceForUniversity(university);
 }
