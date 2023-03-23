cf104dfa87b0b06a10beb510f14ec0b74f0989b1
Roman Panaiot <ramanpan123@gmail.com>
2023-03-23 08:48:31 +0300

    Add constants

---
+++ HierarchicalStructureOfDataStorage/Messages.h
@@ -0,0 +1,16 @@
+#ifndef HIERARCHICALSTRUCTUREOFDATASTORAGE_MESSAGES_H
+#define HIERARCHICALSTRUCTUREOFDATASTORAGE_MESSAGES_H
+
+#include <string>
+
+void showErrorMessage(const std::string &message);
+
+void showInfoMessage(const std::string message);
+
+long validation();
+
+long validationWithArgument(int min, int max);
+
+bool checkOverflow(double d);
+
+#endif //HIERARCHICALSTRUCTUREOFDATASTORAGE_MESSAGES_H
