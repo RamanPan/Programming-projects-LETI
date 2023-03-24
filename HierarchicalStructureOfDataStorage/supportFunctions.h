e776e2b0f7098bb54d13a3499db47359693a4c66
Roman Panaiot <ramanpan123@gmail.com>
2023-03-24 16:27:56 +0300

    Add all supportFunctions

--- HierarchicalStructureOfDataStorage/supportFunctions.h
+++ HierarchicalStructureOfDataStorage/supportFunctions.h
@@ -2,21 +2,22 @@
 #define HIERARCHICALSTRUCTUREOFDATASTORAGE_SUPPORTFUNCTIONS_H
 
 #include "University.h"
+#include <windows.h>
 
-void showMenuForUniversity(int position);
+void showMenuForUniversity(int position, University &university);
 
-void showMenuForFaculty(int position);
+void showMenuForFaculty(int position, Faculty &faculty);
 
-void showMenuForDepartment(int position);
+void showMenuForDepartment(int position, Department &department);
 
-void showMenuForGroup(int position);
+void showMenuForGroup(int position, Group &group);
 
-void consoleInterfaceForUniversity(University& university);
+void consoleInterfaceForUniversity(University &university);
 
-void consoleInterfaceForFaculty(Faculty& faculty);
+void consoleInterfaceForFaculty(Faculty &faculty, University &university);
 
-void consoleInterfaceForDepartment(Department& department);
+void consoleInterfaceForDepartment(Department &department, University &university);
 
-void consoleInterfaceForGroup(Group& group);
+void consoleInterfaceForGroup(Group &group, University &university);
 
 #endif //HIERARCHICALSTRUCTUREOFDATASTORAGE_SUPPORTFUNCTIONS_H
