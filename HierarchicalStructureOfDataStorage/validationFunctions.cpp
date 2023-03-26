7f1f69321a8cb5448e50e5a6fda6cae1c2a84ffb
Roman Panaiot <ramanpan123@gmail.com>
2023-03-26 11:37:33 +0300

    Add validationFunctions

---
+++ HierarchicalStructureOfDataStorage/validationFunctions.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <iostream>
+#include "validationFunctions.h"
+
+std::string validateString(std::string message) {
+    std::string string;
+    showInfoMessage(message);
+    std::cin >> string;
+    while (string.empty()) {
+        showErrorMessage("Строка не может быть пустой");
+        std::cin >> string;
+    }
+    return string;
+}
+
+long validation() {
+    bool validationFlag = false;
+    double result;
+    while (!validationFlag) {
+        if (scanf("%lf", &result)) {
+            if (!checkOverflow(result)) {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+                printf("Неправильный ввод! Число вышло за границу дозволенного! Попробуйте снова!\n");
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+            } else validationFlag = true;
+        } else {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неправильный ввод! Попробуйте снова!\n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+        fflush(stdin);
+    }
+    return (long) result;
+}
+
+long validationWithArgument(int min, int max) {
+    long result;
+    do {
+        result = validation();
+        if (result > max || result < min) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неверный ввод! Значение должно быть от %d до %d\n", min,
+                   max);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+    } while (result > max || result < min);
+    return result;
+}
+
+void choice(char &YN) {
+    bool exit = false;
+    while (!exit) {
+        YN = (char) getchar();
+        fflush(stdin);
+        if (YN == '1' || YN == '2') exit = true;
+        else if (YN != ENTER) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+            printf("Неверный ввод! Значение должно быть либо 1, либо 2 \n");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
+        }
+    }
+}
+
+bool checkOverflow(double d) {
+    return d >= -2147483648 && d <= 2147483647;
+}
---
+++ HierarchicalStructureOfDataStorage/validationFunctions.h
@@ -0,0 +1,21 @@
+#ifndef HIERARCHICALSTRUCTUREOFDATASTORAGE_VALIDATIONFUNCTIONS_H
+#define HIERARCHICALSTRUCTUREOFDATASTORAGE_VALIDATIONFUNCTIONS_H
+
+#include "windows.h"
+#include "constants.h"
+#include "Messages.h"
+#include <string>
+
+
+long validation();
+
+long validationWithArgument(int min, int max);
+
+std::string validateString(std::string message);
+
+void choice(char &YN);
+
+bool checkOverflow(double d);
+
+
+#endif //HIERARCHICALSTRUCTUREOFDATASTORAGE_VALIDATIONFUNCTIONS_H
