afcf2301e3d1fb7fe03bb1f405db9463797d5fa2
Roman Panaiot <ramanpan123@gmail.com>
2023-03-27 11:58:08 +0300

    Output to console done

--- HierarchicalStructureOfDataStorage/Messages.cpp
+++ HierarchicalStructureOfDataStorage/Messages.cpp
@@ -8,40 +8,6 @@ void showErrorMessage(const std::string &message) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
 
-long validation() {
-    bool validationFlag = false;
-    double result;
-    while (!validationFlag) {
-        if (scanf("%lf", &result)) {
-            if (!checkOverflow(result)) {
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-                printf("Неправильный ввод! Число вышло за границу дозволенного! Попробуйте снова!\n");
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-            } else validationFlag = true;
-        } else {
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-            printf("Неправильный ввод! Попробуйте снова!\n");
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-        }
-        fflush(stdin);
-    }
-    return (long) result;
-}
-
-long validationWithArgument(int min, int max) {
-    long result;
-    do {
-        result = validation();
-        if (result > max || result < min) {
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
-            printf("Неверный ввод! Значение должно быть от %d до %d\n", min,
-                   max);
-            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
-        }
-    } while (result > max || result < min);
-    return result;
-}
-
-bool checkOverflow(double d) {
-    return d >= -2147483648 && d <= 2147483647;
+void showInfoMessage(const std::string &message) {
+    std::cout << message << std::endl;
 }
--- HierarchicalStructureOfDataStorage/Messages.h
+++ HierarchicalStructureOfDataStorage/Messages.h
@@ -5,7 +5,7 @@
 
 void showErrorMessage(const std::string &message);
 
-void showInfoMessage(const std::string message);
+void showInfoMessage(const std::string &message);
 
 long validation();
 
