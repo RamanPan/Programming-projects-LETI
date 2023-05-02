78272059ee4b2fdd0971460ad4cbc16e693c000d
Roman Panaiot <ramanpan123@gmail.com>
2023-05-02 11:17:46 +0300

    Declare all functions in client

--- QtClientServerApplication/ClientPart/client.h
+++ QtClientServerApplication/ClientPart/client.h
@@ -8,40 +8,57 @@
 
 QT_BEGIN_NAMESPACE
 class QComboBox;
+
 class QLabel;
+
 class QLineEdit;
+
 class QPushButton;
+
 class QTcpSocket;
+
 QT_END_NAMESPACE
 namespace Ui {
-class Client;
+    class Client;
 }
-//! [0]
-class Client : public QDialog
-{
-    Q_OBJECT
+
+class Client : public QDialog {
+Q_OBJECT
 
 public:
     explicit Client(QWidget *parent = nullptr);
-    ~Client();
+
+    ~Client() override;
+
+private:
+    void initConnections();
 
 private slots:
-    void requestNewFortune();
-    void readFortune();
+
+    void signalInsertDataOn();
+
+    void sendRequestToSelect();
+
+    void sendRequestToInsert();
+
+    void sendRequestToDelete();
+
+    void readResponse();
+
     void displayError(QAbstractSocket::SocketError socketError);
-    void enableGetFortuneButton();
+
 
 private:
     QComboBox *hostCombo = nullptr;
     QLineEdit *portLineEdit = nullptr;
     QLabel *statusLabel = nullptr;
     QPushButton *getFortuneButton = nullptr;
+    QList<QString> *keys;
 
     QTcpSocket *tcpSocket = nullptr;
     QDataStream in;
     QString currentFortune;
-    Ui::Client* ui;
+    Ui::Client *ui;
 };
-//! [0]
 
 #endif
