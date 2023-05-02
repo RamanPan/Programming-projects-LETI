60771c44951f26de2ea127ad29e16e35dc995e06
Roman Panaiot <ramanpan123@gmail.com>
2023-05-02 11:19:11 +0300

    Declare all functions in server

--- QtClientServerApplication/ServerPart/server.h
+++ QtClientServerApplication/ServerPart/server.h
@@ -21,46 +21,57 @@ Q_OBJECT
 public:
     explicit Server(QWidget *parent = nullptr);
 
+    ~Server() override;
+
 private slots:
+
+    void getNextPending();
 
     void processingRequest();
 
-    void sendSelectAll(QTcpSocket *conn);
+    void sendSelectAll();
 
-    void sendSelectWithOneCondition(QTcpSocket *conn, QString key, QString value);
+    void sendSelectWithOneCondition(const QString &key, const QString &value);
 
-    void sendSelectWithTwoCondition(QTcpSocket *conn, QString key1, QString value1, QString key2, QString value2);
+    void
+    sendSelectWithTwoCondition(const QString &key1, const QString &value1, const QString &key2, const QString &value2);
 
-    void sendSelectWithThreeCondition(QTcpSocket *conn, QString key, QString value, QString key2, QString value2,
-                                      QString key3, QString value3);
+    void
+    sendSelectWithThreeCondition(const QString &key1, const QString &value1, const QString &key2, const QString &value2,
+                                 const QString &key3, const QString &value3);
 
-    void sendSelectWithAllCondition(QTcpSocket* conn, QString keyFamily, QString valueFamily,
-                                    QString keyName, QString valueName, QString keyGroup, QString valueGroup,
-                                    QString keyGender, QString valueGender);
+    void sendSelectWithAllCondition(const QString &keyFamily, const QString &valueFamily,
+                                    const QString &keyName, const QString &valueName, const QString &keyGroup,
+                                    const QString &valueGroup,
+                                    const QString &keyGender, const QString &valueGender);
 
-    void sendDeleteAll(QTcpSocket *conn);
+    void sendDeleteAll();
 
-    void sendDeleteWithOneCondition(QTcpSocket *conn, QString key, QString value);
+    void sendDeleteWithOneCondition(const QString &key, const QString &value);
 
-    void sendDeleteWithTwoCondition(QTcpSocket *conn, QString key1, QString value1, QString key2, QString value2);
+    void
+    sendDeleteWithTwoCondition(const QString &key1, const QString &value1, const QString &key2, const QString &value2);
 
-    void sendDeleteWithThreeCondition(QTcpSocket *conn, QString key, QString value, QString key2, QString value2,
-                                      QString key3, QString value3);
+    void
+    sendDeleteWithThreeCondition(const QString &key1, const QString &value1, const QString &key2, const QString &value2,
+                                 const QString &key3, const QString &value3);
 
-    void sendDeleteWithAllCondition(QTcpSocket* conn, QString keyFamily, QString valueFamily,
-                                    QString keyName, QString valueName, QString keyGroup, QString valueGroup,
-                                    QString keyGender, QString valueGender);
+    void sendDeleteWithAllCondition(const QString &key1, const QString &value1,
+                                    const QString &key2, const QString &value2, const QString &key3,
+                                    const QString &value3,
+                                    const QString &key4, const QString &value4);
 
-    void sendInsertData(QTcpSocket* conn,QString valueF,QString valueN,QString valueGroup, QString valueGender);
+    void
+    sendInsertData(const QString &valueF, const QString &valueN, const QString &valueGroup, const QString &valueGender);
 
 private:
     void initServer();
 
     QLabel *statusLabel = nullptr;
     QTcpServer *tcpServer = nullptr;
+    QTcpSocket *conn;
     QList<QString> fortunes;
     QSqlDatabase db;
 };
-//! [0]
 
 #endif
